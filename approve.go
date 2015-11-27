39075e122fe3e3c7ccb5c4e3ae980cc02cd8d42f
Tosh Afanasiev <tosh@tosh.me>
2015-11-27 18:08:12 +1100

    Added the -fixup flag

--- approve.go
+++ approve.go
@@ -5,7 +5,16 @@ import (
 	"path"
 	"runtime"
 	"testing"
+  "flag"
 )
+
+var fixup *bool
+
+func init(){
+  fixup = flag.Bool("fixup", false, "Fixup failing tests by overwriting the approved content")
+
+  flag.Parse()
+}
 
 func approve(t *testing.T, value string) {
 	// program counter, filename, line, ok
@@ -19,7 +28,10 @@ func approve(t *testing.T, value string) {
 
 		if approved, err := ioutil.ReadFile(file); err != nil {
 			t.Logf("Could not read approved value from '%s'", file)
-			ioutil.WriteFile(file, []byte(value), 0755)
+      if *fixup {
+        t.Logf("Fixing up :D")
+        ioutil.WriteFile(file, []byte(value), 0755)
+      }
 			t.Error()
 			return
 		} else {
