aab046ffe5a1568cb2593a73c96a713ae0ab0e53
Tosh Afanasiev <tosh@tosh.me>
2015-11-27 17:30:50 +1100

    Got the calling function name now

--- approve_test.go
+++ approve_test.go
@@ -8,14 +8,18 @@ import (
 func approve(t *testing.T, value string) {
 	pc, fname, line, ok := runtime.Caller(1)
 	if ok {
+
+    caller := runtime.FuncForPC(pc)
 		t.Logf(
-			"You were called by '%s', line %d (program counter %d)",
+			"You were called by %s in '%s', line %d (program counter %d)",
+      caller.Name(),
 			fname,
 			line,
 			pc,
 		)
+
+    t.Error()
 	}
-  t.Error()
 }
 
 func TestRuntimeCaller(t *testing.T) {
